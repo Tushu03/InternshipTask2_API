a584e33ad1dc85dd39c7ab281f9f1a11ad20035c
Tushar Veer <164470037+Tushu03@users.noreply.github.com>
2026-02-05 16:51:16 +0530

    Updated User Entity

--- hello-api/src/user/user.entity.ts
+++ hello-api/src/user/user.entity.ts
@@ -1,11 +1,30 @@
-// src/user/user.entity.ts
+
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+export enum UserRole {
+  PATIENT = 'PATIENT',
+  DOCTOR = 'DOCTOR',
+}
+
+@Entity('users')
 export class User {
+
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
   name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.PATIENT,
+  })
+  role: UserRole;
 }
