6c7ce8fdbb2fa3518197521df818a4abeb9feef5
Tushar Veer <veertushar72@gmail.com>
2026-02-05 16:21:20 +0530

    PostGress Task

--- hello-api/src/app.module.ts
+++ hello-api/src/app.module.ts
@@ -1,10 +1,22 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'testdb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
---
+++ hello-api/src/user/user.entity.ts
@@ -0,0 +1,11 @@
+// src/user/user.entity.ts
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+}
